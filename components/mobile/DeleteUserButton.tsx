ff2632b638a5f214eb5af78a2893bd75b26e7036
ollie-stytch <ollie@stytch.com>
2023-11-14 13:54:18 -0800

    Comment on delete user button

--- components/mobile/DeleteUserButton.tsx
+++ components/mobile/DeleteUserButton.tsx
@@ -4,6 +4,9 @@ import {Button} from "@mui/material";
 import {deleteCurrentUser} from "@/lib/utils";
 
 function DeleteUserButton() {
+    // This exists for testing purposes. This allows for a user to delete their account in the case the user loses access to their passkey,
+    // and they want to take MFA required actions. What this would likely look like in production is the user reaching out to support and
+    // having an admin delete their lost factors.
     const stytch = useStytch();
     const { user } = useStytchUser();
 
