b02cd4db2d30f3869b5d2e7e1c52fa71a89285fe
ollie-stytch <ollie@stytch.com>
2023-11-29 10:17:21 +0000

    copy update

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -311,8 +311,7 @@ function Dashboard() {
                   horizontal: "center"
                 }} open={open} autoHideDuration={20000} onClose={handleClose}>
               <Alert onClose={handleClose} severity="success" sx={{ width: '87.5%' }}>
-                Congratulations on successfully creating a Passkey! To see Passkey authentication in action, just sign out and log back in. If you&apos;re utilizing a cross-device authenticator, explore cross-device authentication by visiting this website on your mobile device and logging in.
-              </Alert>
+                Congratulations on successfully creating a Passkey! To see Passkey authentication in action, just sign out and log back in. Also, if your Passkey is synced, explore cross-device authentication by visiting this website on your mobile device and logging in!</Alert>
             </Snackbar>
           </Box>
         </Box>
